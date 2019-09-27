d0c3c2e318b1598c979dd5e05e0f496a8ba95513
Lingyi <45639148+LingyiC@users.noreply.github.com>
2019-09-27 19:05:20 -0400

    Update process.R

--- lib/process.R
+++ lib/process.R
@@ -3,7 +3,7 @@ df_2015$Latitude <- as.numeric(df$Latitude)
 df_2015$Longitude <- as.numeric(df$Longitude)
 
 sample_data_2015 <- df_2015[c(1:1000),]
-saveRDS(sample_data_2015, "./data/2015_sample_data.rds")
+saveRDS(sample_data_2015, "../data/2015_sample_data.rds")
 
 
 
